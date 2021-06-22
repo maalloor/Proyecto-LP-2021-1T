375e359f3497df9d9fcb1c9cb711b70573b31616
maalloor <maalloor@espol.edu.ec>
2021-06-21 19:06:37 -0500

    Se añadió nuevos tokens (INTEGER, FLOAT), nuevos símbolos (menor que, mayor que, signo de exclamación de cierre, comilla simple, y comilla doble). Además se modificó el nombres de algunas variables y se definieron nuevos módulos que son t_FLOAT y t_INTEGER

--- Interprete_Ruby/venv/Include/intepreteRuby.py
+++ Interprete_Ruby/venv/Include/intepreteRuby.py
@@ -15,51 +15,68 @@ reserved = {
     'and': 'AND',
     'or': 'OR',
     'not': 'NOT',
-    'break': 'BREAK'
+    'break': 'BREAK',
+    'nil': 'NIL'
 }
 symbols = {
     '@': 'AT',
     '#': 'HASH',
     '=': 'EQUAL',
-    '$': 'DOLLAR'
+    '$': 'DOLLAR',
+    '>': 'GREATER',
+    '<': 'LESS',
+    '!': 'EXCLAMATION',
+    '"': 'DQUOTE',
+    "'": 'SQUOTE'
 }
 tokens = (
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
+    'INTEGER',
+    'STRING',
+    'FLOAT',
+    'ADD',
+    'SUBTRACT',
+    'MULTIPLY',
     'DIVIDE',
     'LPAREN',
     'RPAREN',
-    'MOD',
-    'ID',
+    'LBRACKET',
+    'RBRACKET',
+    'MODULUS',
 ) + tuple(reserved.values()) + tuple(symbols.values())
 # Regular expression rules for simple tokens
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
+t_ADD = r'\+'
+t_SUBTRACT = r'-'
+t_MULTIPLY = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_MOD = r'%'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_MODULUS = r'%'
 
-def t_ID(t):
+def t_STRING(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
+    t.type = reserved.get(t.value, 'STRING')
+    return t
+
+def t_FLOAT(t):
+    r'(\d+\.\d*)|(\d*\.\d+)'
+    t.value = float(t.value)
+    return t
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
     return t
 
 def t_SYMBOL(t):
     r'\W'
-    t.type = symbols.get(t.value, 'ID')
+    t.type = symbols.get(t.value, 'STRING')
     if t.type in symbols.values():
         return t
     else:
         t_error(t)
-# A regular expression rule with some action code
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
@@ -71,12 +88,14 @@ t_ignore = ' \t'
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 def getTokens(lexer):
     while True:
         tok = lexer.token()
         if not tok:
             break  # No more input
         print(tok)
+
 # Build the lexer
 lexer = lex.lex()
 linea=" "
